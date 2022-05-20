079f1718ff4701f28555781c8be60873484b6a9f
ASDleves <ricsi20020302@gmail.com>
2022-05-20 12:33:19 +0200

    negyedikcommit

--- Torpedo/src/torpedo/Hajo.java
+++ Torpedo/src/torpedo/Hajo.java
@@ -3,9 +3,17 @@ package torpedo;
 
 public class Hajo {
     int pozicio [];
+
+    Hajo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     public String talalat(int poz){
         return null;
     };
+
+    public Hajo(int[] pozicio) {
+        this.pozicio = pozicio;
+    }
          
 }
--- Torpedo/src/torpedo/TorpedoTeszt.java
+++ Torpedo/src/torpedo/TorpedoTeszt.java
@@ -3,6 +3,10 @@ package torpedo;
 
 public class TorpedoTeszt {
 
+    private static void tesztLoves() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     int poz;
     public static void main(String[] args) {
         tesztLoves();
@@ -11,7 +15,7 @@ public class TorpedoTeszt {
     public String tesztLoves(int poz){
         Hajo hajo = new Hajo();
         String t = hajo.talalat(4);
-        assert t.equals("talált"):"nem jó a találat ellenőrzése";
+        assert t.equals("talált"):"nem jó  a találat ellenőrzése";
         return null;
     };
 }
